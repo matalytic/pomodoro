4ebb4cc969b067c3caabd81229f6dcc5202d6993
Matalytic <matthewaarongo@gmail.com>
2017-03-23 17:07:21 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//Begin with default values in input forms
+
+
+
+//Read data in input forms for session and break
+
+
+//Start button begins timer function
+// To Do: clear old time
+$('#start-btn').on('click', function() {
+	$('.timer').startTimer();
+});
